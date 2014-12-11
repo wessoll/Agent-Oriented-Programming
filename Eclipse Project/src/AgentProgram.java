640f0b684a64ec76e7993f2845e9debd0f92c704
Bono de Visser <bono.de.visser@hva.nl>
2014-12-11 14:34:50 +0100

    Car example works with agent program. Works with arguments

--- Eclipse Project/src/AgentProgram.java
+++ Eclipse Project/src/AgentProgram.java
@@ -1,32 +1,33 @@
-
-
 import jade.core.*;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.StaleProxyException;
 
+
 public class AgentProgram {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Runtime run = Runtime.instance();
-		
-		Profile containerProfile = new ProfileImpl();
-		containerProfile.setParameter(Profile.PLATFORM_ID, "platform");
-		containerProfile.setParameter(Profile.CONTAINER_NAME, "mainContainer");
-		
-		AgentContainer container = run.createMainContainer(containerProfile);
-		
-		try {
-			container.createNewAgent("naam", "Auto", null).start();
-		} catch (StaleProxyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Runtime run = Runtime.instance();
+        
+        Profile containerProfile = new ProfileImpl();
+        containerProfile.setParameter(Profile.PLATFORM_ID, "platform");
+        containerProfile.setParameter(Profile.CONTAINER_NAME, "mainContainer");
+        
+        AgentContainer container = run.createMainContainer(containerProfile);
+        
+        try {
+        	Object[] car_args = new Object[4];
+        	car_args[0] = "car_1";
+        	car_args[1] = 'a';
+        	car_args[2] = new Place();
+        	car_args[3] = new Place();
+        	
+            container.createNewAgent("auto_1", "Car", car_args).start();
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
+    }
 }
