b5e226fdfa4fea7015775cf3176745c0261fc4ad
Bono de Visser <bono.de.visser@hva.nl>
2015-01-06 09:53:35 +0100

    Remove: setup code for car objects. Not sure if these are necesary anymore, because of new way of adding agents to containers.

--- Eclipse Project/src/main/PassengerCar.java
+++ Eclipse Project/src/main/PassengerCar.java
@@ -7,14 +7,6 @@ public class PassengerCar extends Car {
 	
 	public void setup(){
 		Object[] args = getArguments();
-		identifier = 		(String) args[0];
-		speed =				(int)	 args[1];
-		starting_point =	(Vertex) args[2];
-		destination = 		(Vertex) args[3];
-		
-        System.out.println("Ambulance "+ identifier +" initialized with these values:");
-        System.out.println("Speed: "+ speed);
-        System.out.println("Starting point: "+ starting_point.getName());
-        System.out.println("Destination: "+ destination.getName());
+		//todo?
 	}
 }
